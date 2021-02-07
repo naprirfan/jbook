c6be183cd81a9153d1e5a08de9c2912badfd4606
M Irfan <naprirfan@gmail.com>
2021-02-07 15:11:37 +0100

    Add service inside onclick function

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,17 @@
 import * as esbuild from 'esbuild-wasm';
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
+  const ref = useRef<any>();
   const [input, setInput] = useState('');
   const [code, setCode] = useState('');
 
   const startService = async () => {
-    const service = await esbuild.startService({
+    ref.current = await esbuild.startService({
       worker: true,
       wasmURL: '/esbuild.wasm',
     })
-
-    console.log(service);
   }
 
   useEffect(() => {
@@ -20,7 +19,9 @@ const App = () => {
   }, [])
 
   const onClick = () => {
-    console.log(input);
+    if (!ref.current) return;
+
+    console.log(ref.current);
   }
 
   return (
