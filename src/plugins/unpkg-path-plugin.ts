d116a15f407944ef0c9899ce3d530db2c3baa457
M Irfan <naprirfan@gmail.com>
2021-02-21 10:24:23 +0100

    Accomodate nested-module case

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -14,10 +14,11 @@ export const unpkgPathPlugin = () => {
 
         const isRelativePath = args.path.includes('./') || args.path.includes('../');
         if (isRelativePath) {
-          const URLObj = new URL(args.path, `${args.importer}/`);
+          const path = new URL(args.path, `https://unpkg.com${args.resolveDir}/`).href;
+          
           return {
             namespace: 'a',
-            path: URLObj.href,
+            path,
           }
         }
 
@@ -34,16 +35,19 @@ export const unpkgPathPlugin = () => {
           return {
             loader: 'jsx',
             contents: `
-              import message from 'medium-test-pkg';
+              import message from 'nested-test-pkg';
               console.log(message);
             `,
           };
         } 
 
-        const { data } = await axios.get(args.path);
+        const { data, request } = await axios.get(args.path);
+        const { responseURL } = request;
+
         return {
           loader: 'jsx',
           contents: data,
+          resolveDir: new URL('./', responseURL).pathname,
         }
 
       });
