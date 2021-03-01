842c76a7e6b9290ea991fdf74ce6d98e45998062
M Irfan <naprirfan@gmail.com>
2021-03-01 21:25:26 +0100

    Add input code

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -8,7 +8,7 @@ const fileCache = localForage.createInstance({
 
 
  
-export const unpkgPathPlugin = () => {
+export const unpkgPathPlugin = (inputCode: string) => {
   return {
     name: 'unpkg-path-plugin',
     setup(build: esbuild.PluginBuild) {
@@ -41,10 +41,7 @@ export const unpkgPathPlugin = () => {
         if (args.path === 'index.js') {
           return {
             loader: 'jsx',
-            contents: `
-              import React from 'react-select';
-              console.log(React);
-            `,
+            contents: inputCode,
           };
         } 
 
