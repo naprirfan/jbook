89c64a2aae26f76a75365fdef21c5604acff0a57
M Irfan <naprirfan@gmail.com>
2021-07-09 21:54:41 +0200

    Add support for CSS file

--- src/plugins/fetch-plugins.ts
+++ src/plugins/fetch-plugins.ts
@@ -23,16 +23,30 @@ export const fetchPlugin = (inputCode: string) => {
         } 
 
         // Check to see if cache exists
-        const cached = await fileCache.getItem<esbuild.OnLoadResult>(args.path);
-        if (cached) {
-          return cached;
-        }
+        // const cached = await fileCache.getItem<esbuild.OnLoadResult>(args.path);
+        // if (cached) {
+        //   return cached;
+        // }
 
         const { data, request } = await axios.get(args.path);
 
+        const escaped = data
+          .replace(/\n/g, '')
+          .replace(/"/g, '\\"')
+          .replace(/'/g, "\\'");
+
+        const fileType = args.path.match(/css$/) ? 'css' : 'jsx';
+        const contents = fileType === 'css' ? 
+          `
+            const style = document.createElement('style');
+            style.innerText = '${escaped}';
+            document.head.appendChild(style);
+          ` :
+          data;
+
         const result: esbuild.OnLoadResult = {
           loader: 'jsx',
-          contents: data,
+          contents,
           resolveDir: new URL('./', request.responseURL).pathname,
         }
 
